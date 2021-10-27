6e800ce2116299e44cec6f5bde4e8a12a380d929
Kevin Nguyen <kaxuo@hotmail.com>
2021-10-27 11:11:29 +0200

    fixes

--- src/components/Global/Footer/Footer.jsx
+++ src/components/Global/Footer/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
             </Link>
           </div>
           <div>
-            <Link className='reactLink' to='/instructions'>
+            <Link className='reactLink' to='/'>
               INSTRUCTIONS
             </Link>
           </div>
