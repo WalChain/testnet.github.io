e8ff9ac333707f4ae1ad062e857de5a345cac587
Kevin Nguyen <kaxuo@hotmail.com>
2021-10-28 11:32:50 +0200

    checkpoint

--- src/components/LandingPage/Polka/Polka.jsx
+++ src/components/LandingPage/Polka/Polka.jsx
@@ -1,8 +1,8 @@
 import styles from './Polka.module.scss';
 import DownloadIcon from '@mui/icons-material/Download';
 import polkaIcon from '../../../assets/Polka/PolkaIcon.svg';
-// import MenuBookIcon from '@mui/icons-material/MenuBook';
-// import { Link } from 'react-router-dom';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import { Link } from 'react-router-dom';
 
 const Polka = () => {
   return (
@@ -17,14 +17,14 @@ const Polka = () => {
             <br />
             Do not hesitate to follow the instructions if you are lost
           </div>
-          {/* <Link to='instructions' style={{ textDecoration: 'none' }}>
+          <Link to='instructions' style={{ textDecoration: 'none' }}>
             <div className={styles.button}>
               <div className={styles.download}>INSTRUCTIONS</div>
               <div className={styles.dll}>
                 <MenuBookIcon />
               </div>
             </div>
-          </Link> */}
+          </Link>
           <a href='https://polkadot.js.org/extension/' className={styles.button} target='_blank rel="noreferrer" '>
             <div className={styles.download}>DOWNLOAD EXTENSION</div>
             <div className={styles.dll}>
